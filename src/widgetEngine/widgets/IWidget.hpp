0058f2a92eabcb41aeb564e5b291829ce0e50ff7
Antoine <me@antoinebellanger.fr>
2025-02-01 20:51:16 +0100

    feat(IWidget/AWidget): pos setter/getter
    to better manage y pos inside it's module

--- src/widgetEngine/widgets/IWidget.hpp
+++ src/widgetEngine/widgets/IWidget.hpp
@@ -16,10 +16,13 @@ namespace Krell
     {
         public:
             virtual ~IWidget() = default;
+            [[nodiscard]] virtual std::uint8_t getPos() const = 0;
+            virtual void setPos(std::uint8_t) = 0;
             [[nodiscard]] virtual std::string getLegend() const = 0;
             [[nodiscard]] virtual std::size_t getRenderSize() const = 0;
 
         protected:
+            std::uint8_t _pos;
             std::string _legend;
             std::size_t _renderSize = 0;
     };
